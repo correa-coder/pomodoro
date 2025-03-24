110fb1201b10ac1276693f1a25d40eef16fcc8b5
Guilherme Correa <80556636+correa-coder@users.noreply.github.com>
2025-03-24 14:27:15 -0300

    add prettier

--- backup/script.js
+++ backup/script.js
@@ -1,14 +1,14 @@
-function get_seconds(time){
+function get_seconds(time) {
     // extracts seconds from a string in the format MM:SS
     return time.slice(3);
 }
 
-function get_minutes(time){
+function get_minutes(time) {
     // extract minutes from a string in the format MM:SS
     return time.slice(0, 2);
 }
 
-function update_timer(timer_id, interval){
+function update_timer(timer_id, interval) {
     timer = document.getElementById(timer_id);
 
     // getting values from string
@@ -19,56 +19,56 @@ function update_timer(timer_id, interval){
     // console.log(minutes);
     // console.log(timer);
 
-    if(seconds == 0){
-        if(minutes == 0){
+    if (seconds == 0) {
+        if (minutes == 0) {
             clearInterval(interval);
-            console.log("Finished");
-        }else{
+            console.log('Finished');
+        } else {
             seconds = 59;
             minutes--;
-        } 
-    }else{
+        }
+    } else {
         seconds--;
     }
 
     // update HTML
-    timer.innerText = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+    timer.innerText = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 
     // update Page title
     document.title = `Pomodoro | ${timer.innerText}`;
 }
 
-function update_work_timer(){
-    update_timer("work_timer", interval);
+function update_work_timer() {
+    update_timer('work_timer', interval);
 }
 
-function update_shortbreak_timer(){
-    update_timer("shortbreak_timer", short_interval);
+function update_shortbreak_timer() {
+    update_timer('shortbreak_timer', short_interval);
 }
 
-function update_longbreak_timer(){
-    update_timer("longbreak_timer", long_interval);
+function update_longbreak_timer() {
+    update_timer('longbreak_timer', long_interval);
 }
 
 // reset timer
-function reset(timer_id){
+function reset(timer_id) {
     timer = document.getElementById(timer_id);
 
-    default_time = "";
+    default_time = '';
 
-    switch(timer_id){
-        case "work_timer":
-            default_time = "25:00";
+    switch (timer_id) {
+        case 'work_timer':
+            default_time = '25:00';
             break;
-        case "shortbreak_timer":
-            default_time = "05:00";
+        case 'shortbreak_timer':
+            default_time = '05:00';
             break;
-        case "longbreak_timer":
-            default_time = "10:00";
+        case 'longbreak_timer':
+            default_time = '10:00';
     }
 
     timer.innerText = default_time;
 
     // remove the time from the page title
-    document.title = "Pomodoro";
+    document.title = 'Pomodoro';
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import './App.css'
-import TimerProgress from './components/TimerProgress'
+import './App.css';
+import TimerProgress from './components/TimerProgress';
 
 function App() {
-
-  return (
-    <main>
-      <div className="card">
-        {/** Timer display */}
-             <TimerProgress seconds={0} />
-             {/**Controls */}
-             <div className='group' style={{marginTop: '3rem'}}>
-        <button className='btn primary'>Start</button>
-        <button className='btn'>Reset</button>
-             </div>
-      </div>
-    </main>
-  )
+    return (
+        <main>
+            <div className="card">
+                {/** Timer display */}
+                <TimerProgress seconds={0} />
+                {/**Controls */}
+                <div className="group" style={{ marginTop: '3rem' }}>
+                    <button className="btn primary">Start</button>
+                    <button className="btn">Reset</button>
+                </div>
+            </div>
+        </main>
+    );
 }
 
-export default App
+export default App;
